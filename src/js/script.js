f6f6990751223711055544fa1ccf9320fcd2940a
Matvey <adntipin.m9971@gmail.com>
2022-05-30 19:20:29 +0300

    add media for slides

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,39 @@
 $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
-        /* adaptiveHeight: true, */
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slide/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/slide/right.svg"></button>',
+         adaptiveHeight: false, 
+        prevArrow: '<button type="button" class="prev"><img src="icons/slide/left.svg"></button>',
+        nextArrow: '<button type="button" class="next"><img src="icons/slide/right.svg"></button>',
         responsive: [
             {
-              breakpoint: 992,
-              settings: {
-                dots: true,
-                arrows: false,
-              }
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    slidesToShow: 1,
+                    /* centerMode: true,
+                    focusOnSelect: true */
+
+                }
             },
         ]
     });
 });
+
+/* var slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    navPosition: 'bottom',
+    
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+}); */
