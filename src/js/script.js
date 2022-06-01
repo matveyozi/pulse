1ceaac38c91b7e54fe4147cb1a95e3a09052b8a6
Matvey <adntipin.m9971@gmail.com>
2022-06-01 11:59:54 +0300

    add footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
-         adaptiveHeight: false, 
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="prev"><img src="icons/slide/left.svg"></button>',
         nextArrow: '<button type="button" class="next"><img src="icons/slide/right.svg"></button>',
         responsive: [
@@ -18,6 +18,26 @@ $(document).ready(function () {
             },
         ]
     });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+
+    
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
 
 /* var slider = tns({
@@ -37,3 +57,4 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
 }); */
+
