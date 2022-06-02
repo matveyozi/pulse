cf99a5468e91dfdd8e026a907e19b7feb637fe9b
Matvey <adntipin.m9971@gmail.com>
2022-06-02 18:00:48 +0300

    add modal

--- src/js/script.js
+++ src/js/script.js
@@ -25,11 +25,11 @@ $(document).ready(function () {
     });
 
 
-    
+
 
     function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -38,6 +38,24 @@ $(document).ready(function () {
     };
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+    });
+    $('.button_mini').each(function(i){
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    //validate
+    $('.feed-form').validate();
 });
 
 /* var slider = tns({
